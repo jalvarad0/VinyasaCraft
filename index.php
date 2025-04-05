915a4956daa6f3b0eefe7b2bae9979a7d5a83b3c
Juan Alvarado <juan93alvarado@gmail.com>
2025-04-04 20:46:13 -0400

    We needed to make the main file index.php so that it loads by default.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Starter Webpage</title>
-    <link rel="stylesheet" href="css/style.css"> <!-- link to CSS -->
+    <link rel="stylesheet" href="css/stylesheet.css"> 
 </head>
 <body>
     <h1>Choose your workout:</h1>
